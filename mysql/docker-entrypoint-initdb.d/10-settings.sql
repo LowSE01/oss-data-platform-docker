8153b6c68fa8a1ba678d17f6f4f3a1abff956def
LowSE01 <12553995+LowSE01@users.noreply.github.com>
2023-03-25 10:29:11 +0900

    MySQL8の設定とユーザー権限を追加。

---
+++ mysql/docker-entrypoint-initdb.d/10-settings.sql
@@ -0,0 +1,4 @@
+set global collation_connection = utf8mb4_general_ci;
+set global collation_database = utf8mb4_general_ci;
+set global collation_server = utf8mb4_general_ci;
+set global default_collation_for_utf8mb4 = utf8mb4_general_ci;
---
+++ mysql/docker-entrypoint-initdb.d/20-grant_privileges.sql
@@ -0,0 +1,2 @@
+grant all privileges on *.* to 'mysql'@'%';
+flush privileges;
