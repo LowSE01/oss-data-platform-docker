738f07da1cea1f587ee7346fbdca700671d3446f
LowSE01 <12553995+LowSE01@users.noreply.github.com>
2023-04-02 21:30:46 +0900

    PostgreSQLに各種スキーマを作成。

---
+++ postgres/docker-entrypoint-initdb.d/10-create_schemas.sql
@@ -0,0 +1,13 @@
+-- @formatter:off
+
+-- datalake
+drop database if exists "datalake";
+create schema if not exists "datalake";
+
+-- datawarehouse
+drop database if exists "datawarehouse";
+create schema if not exists "datawarehouse";
+
+-- datamart
+drop database if exists "datamart";
+create schema if not exists "datamart";
